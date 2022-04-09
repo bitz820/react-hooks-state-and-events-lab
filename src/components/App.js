564fa58f1b33ddb404c2f38a71ce2eadf9d1c49a
Mark <mark.n.bacon.jr@gmail.com>
2022-04-09 15:59:53 -0400

    commit tree 22f9c71c386c4c87efb457b2e134f4d40028f39b

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light"
+  const text = isDarkMode ? "Light" : "Dark"
+
+  function handleDarkMode(){
+    setIsDarkMode(isDarkMode => !isDarkMode)
+  }
+
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkMode}>{text} Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
   );
 }
+
+
+// const [isDarkMode, setisDarkMode] = useState(false)
+
+// // replace 'false' with a state variable that can be toggled between true and false
+// // this will be used for the Dark Mode Toggle feature
+// const appClass = isDarkMode ? "App dark" : "App light"
+// const mode = isDarkMode ? "Light Mode" : "Dark Mode"
+// function handleDarkMode(){
+//   setisDarkMode((isDarkMode) => !isDarkMode)
+// }
+
+// return (
+//   <div className={appClass}>
+//     <header>
+//       <h2>Shopster</h2>
+//       <button onClick={handleDarkMode}>{mode}</button>
+//     </header>
+//     <ShoppingList items={itemData} />
+//   </div>
+// );
+// }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import items from "../data/items";
+
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
-}
 
-export default Item;
+  const [isInCart, setIsInCart] = useState(false)
+
+  const appClass = isInCart ? 'in-cart' : "";
+  const btnClass = isInCart ? "remove" : "add"
+  const text = isInCart ? "Remove from" : "Add to"
+
+  function handleItem(){
+    setIsInCart(isInCart => !isInCart)
+  }
+  
+    return (
+      <li className={appClass}>
+        <span>{name}</span>
+        <span className="category">{category}</span>
+        <button className={btnClass} onClick={handleItem} >{text} Cart</button>
+      </li>
+    );
+  }
+  
+  export default Item;
+  
+
+// function Item({ name, category }) {
+  
+// const [isInCart, setIsInCart] = useState(false)
+// const itemClass = isInCart ? "in-cart" : ""
+// const buttonText = isInCart ? "Remove from Cart" : "Add to Cart"
+
+// function handleItem(){
+//   setIsInCart((isInCart) => !isInCart)
+//   console.log(isInCart)
+// }
+
+//   return (
+//     <li className={itemClass}>
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button onClick={handleItem} className="add">{buttonText}</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
